f53f08bea1c1331df1e0890dd298912887f18074
AmaurX <amaury.camus92@gmail.com>
2017-04-05 10:07:58 +0200

    pour le pi

--- src/emergencyBreak.cpp
+++ src/emergencyBreak.cpp
@@ -1,37 +1,27 @@
 #include "ros/ros.h"
 #include "emergency/servo_command.h"
-#include <bondcpp/bond.h>
+#include "std_msgs/Bool.h"
 
-#include "std_msgs/String.h"
+ros::Publisher heartbeat; 
+
+void sendHeartBeat(const ros::TimerEvent&){
+	std_msgs::Bool msg;
+	msg.data = true;
+	heartbeat.publish(msg);
+}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "emergencyNode");
   ros::NodeHandle n;
   
-  ros::Publisher bondTimonier = n.advertise<std_msgs::String>("bondTimonier",100);
+  heartbeat = n.advertise<std_msgs::Bool>("bondServo",100);
   ros::Publisher command_pub = n.advertise<emergency::servo_command>("servo_command", 1000);
 	
-	std::string id = "99656856";
+	ros::Timer timer = n.createTimer(ros::Duration(1.0), sendHeartBeat);
+  
+
 	
-	bond::Bond* bondTimo = new bond::Bond("bondTimonier", id);
-	
-	while(true){
-		std_msgs::String msg;
-		msg.data = id.c_str();
-		bondTimonier.publish(msg);
-		bondTimo->start();
-		if (!bondTimo->waitUntilFormed(ros::Duration(1.0))){
-    	ROS_ERROR("timonier unconnected to emergencyBreak");
- 		}
- 		else{
- 			ROS_INFO("timonier connected to emergencyBreak");
- 			break;
- 		}
-		ros::Duration(1.0).sleep();
-	}
-// ... do things with B ...
-	bondTimo->waitUntilBroken(ros::Duration(-1));
-	printf("Timonier has broken the bond\n");
+	ros::spin();
 	return 0;
 }
